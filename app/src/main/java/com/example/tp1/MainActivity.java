fac1da53510b6a21323852ae5212175945d7e458
Jean Jonathan <jeanjonathank2@gmail.com>
2023-02-27 22:29:15 +0100

    Declration des attributs correspondant aux objets graphiques

--- app/src/main/java/com/example/tp1/MainActivity.java
+++ app/src/main/java/com/example/tp1/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.tp1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //2-Declaration des attributs correspondants aux objets graphique
+    private ImageView imageView;
+    private TextView tcompositionMenu;
+    private TextView tEntrees;
+    private Spinner spinEntrees;
+    private TextView tPlat;
+    private Spinner spinPlats;
+    private TextView tDessert;
+    private Spinner spinDessert;
+    private TextView tQuantite;
+    private Spinner spinQuantite;
+    private EditText eQuantite;
+    private Button bAjouter;
+    private TextView tRecaptitulatif;
+    private EditText eRemarques;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialisation des attributs correspondant aux objets graphiques
+
     }
+    //Maintenant on utilise ses utiliser ces variables dans le reste de
+    // notre code pour accéder aux propriétés et méthodes de chaque widget
+
+    //Ajout des methode sneccessaires
+
+
 }
