d8e53f83f3e852f368641762092bf7b3a3101afd
Jean Jonathan <jeanjonathank2@gmail.com>
2023-02-27 23:02:53 +0100

    e lorsque l'utilisateur sélectionne une quantité dans le spinner, la quantité correspondante soit notée dans le champ de saisie manuel.

--- app/src/main/java/com/example/tp1/MainActivity.java
+++ app/src/main/java/com/example/tp1/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.tp1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -44,16 +48,42 @@ public class MainActivity extends AppCompatActivity {
         tDessert = findViewById(R.id.tDessert);
         spinDessert = findViewById(R.id.spinDessert);
         tQuantite = findViewById(R.id.tQuantite);
+
         spinQuantite = findViewById(R.id.spinQuantite);
         eQuantite = findViewById(R.id.eQuantite);
+
         bAjouter = findViewById(R.id.bAjouter);
         tRecaptitulatif = findViewById(R.id.tRecaptitulatif);
         eRemarques = findViewById(R.id.eRemarques);
+
+
+        //Ajout de l'ecouteur d'evenement onItemSelected
+        //Pour cela on a besoin de deux info, la quantite selectionne et l'edit text quantite
+
+        //Mise en place de l'ecouteur d'evemment sur le spinner spinQuantite
+        spinQuantite.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+           //Creation de l'ecouteur d'evenement onItemSlected
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //On declare quantiteSelectionne pour reccuperer la quantite selectionner sous forme de Strign
+                // lorsque l'utilisateur declenche l'evenement
+                String quantiteSelectionne = adapterView.getItemAtPosition(i).toString();//getItemAtPosition pour reccuperer la quantite selectionner
+                eQuantite.setText(quantiteSelectionne); // Mise a jour dans le champ de saisie manule en utiliant setText
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //Alors on ne fait rien
+            }
+        });
+
+
+
     }
+    //Declaration eventuelle de methodes
+
+
     //Maintenant on utilise ses utiliser ces variables dans le reste de
     // notre code pour accéder aux propriétés et méthodes de chaque widget
-
-    //Ajout des methode sneccessaires
-
 
 }
